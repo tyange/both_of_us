ca61527be254023a93c17737575202b82e37aa25
tyange <usun16@gmail.com>
2023-09-09 23:16:18 +0900

    add anniversary card

---
+++ lib/widgets/anniversary_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AnniversaryCard extends StatelessWidget {
+  const AnniversaryCard({super.key, required String date, required String});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('card'),
+    );
+  }
+}
